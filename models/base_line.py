113a3eda8dd6f986770c30bf5a9c9a430a156dbb
GAtemROU <timur.mikhalevsky@gmail.com>
2024-03-11 18:24:06 +0100

    added f1score metric

--- models/base_line.py
+++ models/base_line.py
@@ -1,15 +1,16 @@
 import torch.nn as nn
 
-class default_cnn(nn.Module):
+
+class BasicModel(nn.Module):
     def __init__(self, in_size, out_size):
         super().__init__()
         self.conv1 = nn.Conv2d(3, in_size, 3, 1)
-        self.conv2 = nn.Conv2d(in_size, in_size*2, 5, 2)
+        self.conv2 = nn.Conv2d(in_size, in_size * 2, 5, 2)
         self.maxpool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         self.relu = nn.ReLU()
-        self.fc = nn.Linear(in_size*2*15*15, out_size) #TODO
+        self.fc = nn.Linear(in_size * 2 * 15 * 15, out_size)  # TODO
         self.flat = nn.Flatten()
-        
+
     def forward(self, X):
         X = self.conv1(X)
         X = self.relu(X)
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,33 +1,49 @@
 import numpy as np
 import torch
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, f1_score
 
 
 def compute_confusion_matrices(model, dataset, participants):
     cfs = {}
     model.eval()
-    for p in participants:
-        y_true = []
-        y_pred = []
-        loader = dataset.get_loader([p])
-        for img, lab in loader:
-            y_true.append(lab.detach().numpy())
-            _, out = torch.max(model.forward(img), 1)
-            out = out.detach().numpy()
-            y_pred.append(out)
-        cfs[p] = confusion_matrix(np.array(y_true).flatten(), np.array(y_pred).flatten())
+    with torch.no_grad():
+        for p in participants:
+            y_true = []
+            y_pred = []
+            loader = dataset.get_loader([p])
+            for img, lab in loader:
+                y_true.append(lab.detach().numpy())
+                _, preds = torch.max(model.forward(img), 1)
+                y_pred.append(preds.detach().numpy())
+            cfs[p] = confusion_matrix(np.array(y_true).flatten(), np.array(y_pred).flatten())
     return cfs
 
 
 def compute_accuracy(model, dataset, participants):
     model.eval()
-    loader = dataset.get_loader(participants)
-    test_accuracy = 0.0
-    test_samples = 0
-    for batch_input, batch_target in loader:
-        batch_out = model.forward(batch_input)
-        _, predicted = torch.max(batch_out, 1)
-        test_accuracy += (predicted == batch_target).sum().item()
-        test_samples += batch_target.size(0)
+    with torch.no_grad():
+        loader = dataset.get_loader(participants)
+        test_accuracy = 0.0
+        test_samples = 0
+        for img, lab in loader:
+            out = model.forward(img)
+            _, preds = torch.max(out, 1)
+            test_accuracy += (preds == lab).sum().item()
+            test_samples += lab.size(0)
     test_accuracy /= test_samples
     return test_accuracy
+
+
+def compute_f1score(model, dataset, participants):
+    loader = dataset.get_loader(participants)
+    all_true_labels = []
+    all_pred_labels = []
+
+    with torch.no_grad():
+        for img, lab in loader:
+            outputs = model(img)
+            _, preds = torch.max(outputs, 1)
+            all_true_labels.extend(lab.detach().numpy())
+            all_pred_labels.extend(preds.detach().numpy())
+    f1 = f1_score(all_true_labels, all_pred_labels, average='weighted')
+    return f1
